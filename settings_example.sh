b179e1724f3a841fbaa072f8779176bff02b688a
J-Donald Tournier <jdtournier@gmail.com>
2018-03-19 21:17:07 +0000

    updated build with a bug fixes and enhancements

---
+++ settings_example.sh
@@ -0,0 +1,18 @@
+# Use this file to override any settings required
+
+# compiler executable:
+#CXX=clang++      # <== use on macOS
+#CXX=g++-7
+
+# any flags common to both optimised and debug builds:
+#cflags="$(pkg-config --cflags eigen3)"
+#ldflags=""
+
+# compiler flags used for optimised build:
+#cflags_optim="-O3 -DNDEBUG"
+#ldflags_optim=""
+
+# compiler flags used for debug build:
+#cflags_debug="-g"
+#ldflags_debug="-g"
+
